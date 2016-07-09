261ed6ba3e4ffc92b9cbba06ffa7f14017755c33
Bill Reimers <bill@wmrnet.com>
2016-07-09 13:21:10 -0400

    Update logout code

--- logout.php
+++ logout.php
@@ -1,15 +1,16 @@
-<?
-session_start();
-$_SESSION['userId']="";
-$_SESSION['userStat']="";
-$_SESSION['userType']="";
-$_SESSION['db']="";
-
-session_destroy();
-
- header('Location: ../index.html');
-
-//echo "<br><br><p>You have been logged out</p>
-//<p><a href='index.html' title='Logout'>Click here to login.<br></a>";
-
-?>
+<?
+session_start();
+$_SESSION['userId']="";
+$_SESSION['userStat']="";
+$_SESSION['userType']="";
+$_SESSION['db']="";
+
+session_destroy();
+
+//header('Location: index.html');
+ header("Location: http://localhost:81/index.html", true);
+
+//echo "<br><br><p>You have been logged out</p>
+//<p><a href='index.html' title='Logout'>Click here to login.<br></a>";
+
+?>
